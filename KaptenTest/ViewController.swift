c707466ed86c2a7ca76045d132a774bf97603299
Artem Sidorenko <artem.sidorenko@21buttons.com>
2019-10-09 15:32:33 +0200

    Initial Commit

---
+++ KaptenTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  KaptenTest
+//
+//  Created by Artem Sidorenko on 09/10/2019.
+//  Copyright © 2019 Artem Sidorenko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
